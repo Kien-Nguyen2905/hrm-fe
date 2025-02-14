2e4427525b00f6051b21799394a4db022710c026
Daisy <kiennguyen12.com@gmail.com>
2025-02-14 22:16:35 +0700

    config ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,22 @@
+require('dotenv').config();
+
 module.exports = {
   apps: [
     {
-      name: process.env.DEVELOPMENT_NAME || process.env.PRODUCTION_NAME,
+      name:
+        process.env.NODE_ENV === 'production'
+          ? process.env.PRODUCTION_NAME
+          : process.env.DEVELOPMENT_NAME,
       script: 'npm run start',
       env: {
-        PORT: process.env.DEVELOPMENT_PORT,
+        NODE_ENV: 'development',
+        PORT: process.env.DEVELOPMENT_PORT || 3000,
+        NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
       },
       env_production: {
-        PORT: process.env.PRODUCTION_PORT,
+        NODE_ENV: 'production',
+        PORT: process.env.PRODUCTION_PORT || 3002,
+        NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT_PROD,
       },
     },
   ],
